1ff92f193dd87b40fa1e758ce1da8c1203cabd48
Patrick <pschmidt@pasit.nl>
2023-02-26 20:21:38 +0100

    init

---
+++ Exercises/Bonus/BonusOpgave/Philips/Lamp.cs
@@ -0,0 +1,16 @@
+﻿using IEEE;
+
+namespace Philips;
+
+public class Lamp: IDevice
+{
+    public void Aan()
+    {
+        Console.WriteLine("De lamp gaat branden");
+    }
+
+    public void Activate()
+    {
+        Aan();
+    }
+}
